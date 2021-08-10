13dbdcb615c5fd5ebd01df4389dbf771520e0db2
ligia-davila-bozzi <ligia@respondeai.com.br>
2021-08-10 10:36:26 -0300

    preparando review

--- scripts/helpers/object-utils.js
+++
@@ -1,8 +0,0 @@
-const ObjectUtils = {
-    isEqual: (object1, object2) => {
-        if (JSON.stringify(object1) === JSON.stringify(object2)){
-            return true;
-        }
-        return false;
-    }
-}
